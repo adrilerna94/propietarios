8e4b15c2db7f8923315086d931beb767015cab3d
adrilerna94 <adriaud@gmail.com>
2025-11-13 17:16:47 +0100

    investment and rentwithstyle done

---
+++ propietarios/app/landlords/icons/Investment-background-top.tsx
@@ -0,0 +1,18 @@
+export function BackgroundTop({ className = "", fill = "#CFE0DC" }) {
+    return (
+        <svg
+        className={className}
+        width="1440"
+        height="496"
+        viewBox="0 0 1440 496"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+      >
+        <path
+          d="M-2 0C-2 0 287 138.5 691 138.5C1078.5 138.5 1442 0 1442 0V495.5H-2V0Z"
+          fill={fill}
+        />
+      </svg>
+    );
+}
